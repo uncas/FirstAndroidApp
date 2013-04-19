4efc602c28e6830a3c0788ba81d0eb458681125f
Ole Lynge Soerensen <olelynge@gmail.com>
2013-04-19 11:22:40 +0200

    Add second activity that displays text entered in first.

--- src/dk/uncas/firstandroidapp/MainActivity.java
+++ src/dk/uncas/firstandroidapp/MainActivity.java
@@ -1,11 +1,16 @@
 package dk.uncas.firstandroidapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	public final static String EXTRA_MESSAGE = "dk.uncas.firstandroidapp.MESSAGE";
+		
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,4 +25,11 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    public void sendMessage(View view) {
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editText = (EditText) findViewById(R.id.edit_message);
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent);
+    }
 }
